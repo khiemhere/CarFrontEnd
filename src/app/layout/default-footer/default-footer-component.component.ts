6819e730a312aa28ecab0819110a5549b0e24ebc
khiemna <duonghongkhiem.dev@gmail.com>
2025-10-21 00:27:28 +0700

    backup before Angular upgrade

---
+++ src/app/layout/default-footer/default-footer-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-footer-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-footer-component.component.html',
+  styleUrl: './default-footer-component.component.scss'
+})
+export class DefaultFooterComponentComponent {
+   
+}
---
+++ src/app/layout/default-header/default-header-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-header-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-header-component.component.html',
+  styleUrl: './default-header-component.component.scss'
+})
+export class DefaultHeaderComponentComponent {
+
+}
